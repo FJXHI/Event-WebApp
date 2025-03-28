d7733bf4bc86f2dc987593b2e04b1250569eeccc
FJXHI <92821848+FJXHI@users.noreply.github.com>
2025-03-28 16:32:05 +0000

    Deploying to gh-pages from @ FJXHI/Event-WebApp@d346b68881b6e74420f713605fc982de7de0197d 🚀

---
+++ assets/ActsView-kDJT49lP.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./ActList.vue_vue_type_script_setup_true_lang-DCIeD3i7.js";import{d as o,c as t,f as r,o as a}from"./index-Dw6tXvBF.js";import"./useEventData-D-4Kz0sm.js";import"./FavBtn-BXm8deiE.js";import"./IconHeart-BK5z9qHr.js";const l=o({__name:"ActsView",setup(c){return(m,n)=>(a(),t("div",null,[r(e)]))}});export{l as default};
