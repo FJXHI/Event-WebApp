d473151f969bd8a9100188a8656507151971426a
FJXHI <92821848+FJXHI@users.noreply.github.com>
2025-06-06 07:03:12 +0000

    Deploying to gh-pages from @ FJXHI/Event-WebApp@67244ab005962be2d5baf5f9bce167b2bf731de9 🚀

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,6 @@
 // firebase-messaging-sw.js
+console.log('Firebase Messaging Service Worker geladen.');
+
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
